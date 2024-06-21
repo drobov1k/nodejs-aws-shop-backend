3067d870fde915264e881f1303795889ff16477c
Yauheni Dubovik <yauheni_dubovik@epam.com>
2024-06-21 15:41:50 +0400

    feat(TASK-4): dynamo db integration

--- services/products/functions/validators/validateCreateProductBody.ts
+++ services/products/functions/validators/validateCreateProductBody.ts
@@ -14,6 +14,6 @@ export function validateCreateProductBody(body?: string): string | undefined {
   const { title, price, count } = dto;
 
   if (!title) return 'Product title should be present';
-  if (price == null || !Number.isInteger(price)) return 'Product price should be integer';
-  if (count == null || !Number.isInteger(count)) return 'Product count should be integer';
+  if (price == null || !Number.isInteger(price) || price < 0) return 'Product price should be positive integer';
+  if (count == null || !Number.isInteger(count) || count < 1) return 'Product count should be positive integer';
 }
