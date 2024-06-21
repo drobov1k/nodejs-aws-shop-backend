3cbdb8408743a642586d302ba29fb738bb2cc961
Yauheni Dubovik <yauheni_dubovik@epam.com>
2024-06-21 19:18:37 +0400

    refactor(refactor-1): remove services

--- services/products/functions/getProductsList.ts
+++ services/products/functions/getProductsList.ts
@@ -1,15 +1,15 @@
 import HttpStatus from 'http-status';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { productService } from '@core/services';
+import { productRepository } from '@core/repositories';
 import { withCors } from '@core/helpers/withCors';
 
 export const getProductsList = withCors(
   async (_event: APIGatewayProxyEvent, _ctx?: Context): Promise<APIGatewayProxyResult> => {
-    const products = await productService.getAll();
+    const products = await productRepository.findAll();
 
     return {
       statusCode: HttpStatus.OK,
       body: JSON.stringify(products),
     };
-  }
+  },
 );
